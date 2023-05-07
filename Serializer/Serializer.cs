a9772d8c6ff932330d663ea545ee3955ce926315
andrw04 <sivyandrey2021@gmail.com>
2023-05-07 15:08:41 +0300

    Added Json Serializer/Deserializer

---
+++ Serializer/Serializer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Serializer
+{
+    public static class Serializer<T>
+    {
+        public static async Task<string> SerializeJSON(T obj)
+        {
+            return await Task.Run(() =>
+            {
+                string json = JsonConvert.SerializeObject(obj);
+                return json;
+            });
+        }
+
+        public static async Task<T?> DeserializeJSON(string json)
+        {
+            return await Task.Run(() =>
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            });
+        }
+    }
+}
